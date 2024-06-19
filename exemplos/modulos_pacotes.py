c53ac310ce23823626fde0eff723edb289d00c09
Marcelo Diedrich <mar@celo.net.br>
2024-06-18 23:59:33 -0300

    Inclusão de pasta com exemplos - Via upload

---
+++ exemplos/modulos_pacotes.py
@@ -0,0 +1,7 @@
+# Exemplo de utilização de módulos em Python
+import math
+
+# Usando funções do módulo math
+raio = 5
+area_circulo = math.pi * (raio ** 2)
+print(f"A área de um círculo com raio {raio} é: {area_circulo:.2f}")
